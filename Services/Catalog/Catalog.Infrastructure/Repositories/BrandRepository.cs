b9e428f15c4f3fd8ab6d7dccba9d6814df976d52
kartavya <kartavyas@gmail.com>
2026-09-06 14:13:46 +1000

    DB crud oprations added in Infrastructure layer

---
+++ Services/Catalog/Catalog.Infrastructure/Repositories/BrandRepository.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.Infrastructure.Repositories
+{
+    public class BrandRepository : IBrandRepository
+    {
+        private readonly IMongoCollection<ProductBrand> _brands;
+        public BrandRepository(IConfiguration config)
+        {
+            var client = new MongoClient(config["DatabaseSettings:ConnectionString"]);
+            var db = client.GetDatabase(config["DatabaseSettings:DatabaseName"]);
+            _brands = db.GetCollection<ProductBrand>(config["DatabaseSettings:BrandCollectionName"]);
+        }
+        public async Task<IEnumerable<ProductBrand>> GetAllBrands()
+        {
+            return await _brands.Find(_ => true).ToListAsync();
+        }
+
+        public async Task<ProductBrand> GetBrandAsync(string id)
+        {
+            return await _brands.Find(x => x.Id == id).FirstOrDefaultAsync();
+        }
+    }
+}
