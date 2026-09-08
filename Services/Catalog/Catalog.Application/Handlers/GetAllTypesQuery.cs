ae43e93d6cd7300882cca9064b7a4e40d85541e1
kartavya <kartavyas@gmail.com>
2026-09-08 17:18:42 +1000

    Getall Brand and Types query, hander and mapper added

---
+++ Services/Catalog/Catalog.Application/Handlers/GetAllTypesQuery.cs
@@ -0,0 +1,9 @@
+﻿using Catalog.Application.Responses;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public record GetAllTypesQuery: IRequest<IList<TypesResponse>>
+    {
+    }
+}
